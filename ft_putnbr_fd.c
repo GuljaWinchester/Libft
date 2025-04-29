130531ae7c79f69deb51e94f6389bd1a90b4b476
Galina Perednya <gperedny@c2r3p8.hive.fi>
2025-04-29 19:26:04 +0300

    first version
    created new functions and added them

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gperedny <gperedny@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 19:12:26 by gperedny          #+#    #+#             */
+/*   Updated: 2025/04/29 19:22:58 by gperedny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	nb;
+	char	c;
+
+	nb = n;
+	if (nb < 0)
+	{
+		nb = -nb;
+		write(fd, "-", 1);
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+	}
+	c = (nb % 10) + '0';
+	write(fd, &c, 1);
+}
+int	main(void)
+{
+	int	NB;
+
+	NB = -2147483648;
+	ft_putnbr_fd(NB, 1);
+	return (0);
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gperedny <gperedny@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 18:17:14 by gperedny          #+#    #+#             */
+/*   Updated: 2025/04/29 18:59:43 by gperedny         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(const char *s, char (*f)(unsigned int, char))
+{
+	char *res;
+	unsigned int i;
+
+	res = malloc((sizeof(char *)) * ft_strlen(s));
+	if (!res)
+		return (NULL);
+	while (i < ft_strlen(s))
+	{
+		res[i] = (*f)(i, s[i]);
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
